dd40f66c8969edcacb4d01fd4d3a18aaead9ef6b
Marcin <markar.mormar@gmail.com>
2019-05-05 18:15:23 +0200

    Add features for capitalization and number of capitalization

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -5,13 +5,12 @@ const initialState = {
   couponRate: 0,
   numberCapitalizationPerYera: 0,
   periodOfTheDeposit: 0,
-  finalCapital: '',
-  net: '',
-  gross: '',
-  tax: ''
+  finalCapital: "",
+  net: "",
+  gross: "",
+  tax: "",
+  period: ""
 };
-
-
 
 const Depoosit = () => {
   const [seedMoney, setSeedMoney] = useState(initialState.seedMoney);
@@ -24,10 +23,16 @@ const Depoosit = () => {
     initialState.periodOfTheDeposit
   );
 
-  const [finalCapital, setFinalCapital] = useState(initialState.finalCapital)
-  const [net, setNet] = useState(initialState.net)
-  const [gross, setGross] = useState(initialState.gross)
-  const [tax, setTax] = useState(initialState.tax)
+  const [finalCapital, setFinalCapital] = useState(initialState.finalCapital);
+  const [net, setNet] = useState(initialState.net);
+  const [gross, setGross] = useState(initialState.gross);
+  const [tax, setTax] = useState(initialState.tax);
+
+  const [periodMenu, setPeriodMenu] = useState(false);
+  const [numOfCapMenu, setNumOfCapMenu] = useState(false);
+  const [calculate, setCalculate] = useState(false);
+
+  const [period, setPeriod] = useState(initialState.period);
 
   function count(
     seedMoney,
@@ -36,46 +41,47 @@ const Depoosit = () => {
     periodOfTheDeposit
   ) {
     console.log("*******");
+    console.log("period: ", period);
     // console.log("seedMoney typeof ", typeof seedMoney);
     // console.log("seedMoney ", seedMoney);
     // console.log("seedMoney parseFloat ", parseFloat(seedMoney));
     // console.log("seedMoney typeof ", typeof seedMoney);
-  
+
     const _seedMoney = parseFloat(seedMoney);
     // console.log("_seedMoneyest", _seedMoney);
     // console.log("_seedMoney ", typeof _seedMoney);
-  
+
     // console.log("seedMoney", typeof(couponRate));
     // console.log("seedMoney", couponRate);
-  
+
     // console.log("couponRate", couponRate);
     // console.log("numberCapitalizationPerYera", numberCapitalizationPerYera);
     // console.log("periodOfTheDeposit", periodOfTheDeposit);
-  
+
     const finalCapital =
       _seedMoney *
       Math.pow(
         1 + couponRate / 100 / numberCapitalizationPerYera,
-        numberCapitalizationPerYera * periodOfTheDeposit
+        (numberCapitalizationPerYera * periodOfTheDeposit) / period
       );
-  
+
     const tax = (finalCapital - _seedMoney) * 0.19;
     const profit = finalCapital - (_seedMoney + tax);
-  
+
     const gross = (finalCapital - _seedMoney).toFixed(2);
     const net = profit.toFixed(2);
     const finalTax = tax.toFixed(2);
-  
+
     const sum = parseFloat(net) + parseFloat(finalTax);
     const grossNum = parseFloat(gross);
     const final = finalCapital.toFixed(2);
-  
+
     if (grossNum === sum) {
       console.log("OK");
     } else {
       console.log("Wrong");
     }
-  
+
     console.log("finalCapital: ", finalCapital);
     console.log("final: ", final);
     console.log("tax: ", tax);
@@ -84,28 +90,28 @@ const Depoosit = () => {
     console.log("net: ", net);
     console.log("finalTax: ", finalTax);
     console.log("sum: ", sum);
-  
+
     setFinalCapital(final);
     setNet(net);
     setGross(gross);
     setTax(finalTax);
 
-    
     return finalCapital;
   }
-
 
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault(); // Zapobiega odświezaniu strony po przesłaniu formularza
-          count(
-            seedMoney,
-            couponRate,
-            numberCapitalizationPerYera,
-            periodOfTheDeposit
-          );
+          if (calculate) {
+            count(
+              seedMoney,
+              couponRate,
+              numberCapitalizationPerYera,
+              periodOfTheDeposit
+            );
+          }
         }}
       >
         <div>
@@ -126,13 +132,32 @@ const Depoosit = () => {
         </div>
         <div>
           <label>
-            Number of capitalization per yera - Liczba kapitalizacji w roku{" "}
+            Number of capitalization per yera - Liczba kapitalizacji w roku
           </label>
-          <input
-            type="number"
-            onChange={e => setNumberCapitalizationPerYera(e.target.value)}
-            value={numberCapitalizationPerYera}
-          />
+          <div>
+            <button onClick = {() => setNumOfCapMenu(!numOfCapMenu)}>Number of capitalization</button>
+            {numOfCapMenu ? (
+              <ul>
+                <li onClick={() => setNumberCapitalizationPerYera(1)}>
+                  At the end of the period
+                </li>
+                <li onClick={() => setNumberCapitalizationPerYera(1)}>
+                  Annual
+                </li>
+                <li onClick={() => setNumberCapitalizationPerYera(12)}>
+                  Monthly
+                </li>
+                <li onClick={() => setNumberCapitalizationPerYera(365)}>
+                  Daily
+                </li>
+              </ul>
+            ) : null}
+
+            {/* {console.log(
+              "numberCapitalizationPerYera: ",
+              numberCapitalizationPerYera
+            )} */}
+          </div>
         </div>
         <div>
           <label>Period of the deposit - okres depozytu </label>
@@ -141,20 +166,42 @@ const Depoosit = () => {
             onChange={e => setPeriodOfTheDeposit(e.target.value)}
             value={periodOfTheDeposit}
           />
+          <div>
+            <button onClick={() => setPeriodMenu(!periodMenu)}>
+              Capitalization
+            </button>
+
+            {periodMenu ? (
+              <div className="menu">
+                <li onClick={() => setPeriod(365)}>Days</li>
+                <li onClick={() => setPeriod(12)}>Months</li>
+                <li onClick={() => setPeriod(1)}>Years</li>
+              </div>
+            ) : (
+              <div />
+            )}
+          </div>
         </div>
+        {console.log("periodMenu", periodMenu)}
         <div>
-          <input type="submit" value="Submit" />
+          <input
+            type="submit"
+            value="Submit"
+            onClick={() => setCalculate(!calculate)}
+          />
         </div>
       </form>
-        {console.log(seedMoney)}
-        <h1>Final Capital : {finalCapital === "" ? 0 : finalCapital }</h1>
-        <h2>Profit: {net === "" ? 0 : net}</h2>
-        <h2>Tax: {tax === "" ? 0 : tax}</h2>
-        <h2>Gross: {gross === "" ? 0 : gross}</h2>
+      {/* {console.log(seedMoney)} */}
+      {/* {console.log("period: ", period)} */}
+
+      <h1>Final Capital : {finalCapital === "" ? 0 : finalCapital}</h1>
+      <h2>Profit: {net === "" ? 0 : net}</h2>
+      <h2>Tax: {tax === "" ? 0 : tax}</h2>
+      <h2>Gross: {gross === "" ? 0 : gross}</h2>
 
       {/* {console.log(seedMoney)} */}
-        {/* <h1>{count(seedMoney, couponRate, numberCapitalizationPerYera, periodOfTheDeposit)}</h1> */}
-        {/* <button>Submit</button> */}
+      {/* <h1>{count(seedMoney, couponRate, numberCapitalizationPerYera, periodOfTheDeposit)}</h1> */}
+      {/* <button>Submit</button> */}
     </div>
   );
 };
